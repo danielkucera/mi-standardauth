c8019412b75faba1def33d408b44e2de86502809
Daniel Kucera <daniel.kucera@gmail.com>
2020-03-11 20:35:15 +0100

    added add_key_handler, parametrized commands

--- provision.py
+++ provision.py
@@ -25,7 +25,14 @@ def scan():
 		        #print "  %s = %s, %s" % ( desc, value, rawval)
 
 setup_data = "\x01\x00"
+CMD_GET_INFO = "\xa2\x00\x00\x00"
+CMD_SET_KEY  = "\x15\x00\x00\x00"
+RCV_RDY = "\x00\x00\x01\x01"
+RCV_OK  = "\x00\x00\x01\x00"
+RCV_TOUT= "\x00\x00\x01\x05\x01\x00"
 
+GET_INFO_STATE = 0
+SET_KEY_STATE = 1
 
 class MiProvision(btle.DefaultDelegate):
     def __init__(self, mac):
@@ -33,6 +40,7 @@ class MiProvision(btle.DefaultDelegate):
         self.p = btle.Peripheral(mac)
         self.p.setDelegate(self)
         self.frames = 0
+        self.state = GET_INFO_STATE
 
     def handleNotification(self, cHandle, data):
         # ... perhaps check cHandle
@@ -40,13 +48,29 @@ class MiProvision(btle.DefaultDelegate):
         frm = ord(data[0]) + 0x100 * ord(data[1])
         print("frm", frm)
         print(data.encode("hex"))
+        if self.state == GET_INFO_STATE:
+            self.info_state_hdlr(frm, data)
+        if self.state == SET_KEY_STATE:
+            self.set_key_hdlr(frm, data)
+
+    def info_state_hdlr(self, frm, data):
         if frm == 0:
             self.frames = ord(data[4]) + 0x100 * ord(data[5])
             print("expecting",self.frames,"frames")
-            self.bt_write(service_uuid, step1, False, "\x00\x00\x01\x01")
+            self.bt_write(service_uuid, step1, False, RCV_RDY)
         if frm == self.frames:
             print("All frames received")
-            self.bt_write(service_uuid, step1, False, "\x00\x00\x01\x00")
+            self.bt_write(service_uuid, step1, False, RCV_OK)
+            self.state = SET_KEY_STATE
+            self.bt_write(service_uuid, step1plus, False, CMD_SET_KEY)
+            self.bt_write(service_uuid, step1, False, "\x00\x00\x00\x03\x04\x00")
+
+    def set_key_hdlr(self, frm, data):
+        if frm == 0:
+            if data == RCV_RDY:
+                print("Mi ready to receive key")
+            if data == RCV_TOUT:
+                print("Key send timeout")
 
     def bt_write(self, serv, char, resp, data):
         svc = self.p.getServiceByUUID(serv)
@@ -59,7 +83,7 @@ class MiProvision(btle.DefaultDelegate):
     
         self.bt_write(service_uuid, step1, True, "\x01\x00")
         self.bt_write(service_uuid, step1plus, True, "\x01\x00")
-        self.bt_write(service_uuid, step1plus, False, "\xa2\x00\x00\x00")
+        self.bt_write(service_uuid, step1plus, False, CMD_GET_INFO)
     
         while True:
             if self.p.waitForNotifications(1.0):
